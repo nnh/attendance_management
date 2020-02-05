644da466555dd022ed7f9ab9343ca51441ff64d8
kirimichan <mariko.ohtsuka@nnh.go.jp>
2020-02-05 13:15:26 +0900

    Change output to tab-delimited text file

--- attendance_management.R
+++ attendance_management.R
@@ -1,6 +1,6 @@
 # attendance_management
 # Mariko Ohtsuka
-# 2019/10/17
+# 2020/02/05
 # ------ set date if necessary ------
 #target_yyyymm <- "201906"
 # ------ libraries ------
@@ -28,7 +28,7 @@ ReadCsvFunc <- function(input_csv){
 #' @example
 GroupbyName <- function(input_df){
   output_df <- data.frame(名前=input_df["name"],
-                          年月日=input_df["ymd"],
+                          ymd=input_df["ymd"],
                           入室=input_df["clockin"],
                           退室=input_df["clockout"],
                           stringsAsFactors=F)
@@ -56,7 +56,6 @@ GetMonthLastDate <- function(yyyymm){
 here() %>% setwd()
 setwd("..")
 input_path <- str_c(getwd(), "/ログデータ")
-output_path <- input_path
 if (exists("target_yyyymm")){
   yyyymm <- target_yyyymm
 } else{
@@ -64,13 +63,17 @@ if (exists("target_yyyymm")){
   yyyymm <- Sys.Date() %>% format("%Y-%m-01") %>% as.Date() %>% {. - 1} %>% format("%Y%m")
 }
 input_yyyymm <- str_c(input_path, "/", yyyymm)
-output_path <- input_yyyymm
-file_list <- list.files(input_yyyymm, full.names=T)
+output_path <- str_c(input_yyyymm, "/output")
+if (file.exists(output_path) == F) {
+  dir.create(output_path)
+}
+file_list <- list.files(input_yyyymm, full.names=T, pattern="^.*.csv")
 df_attendance <- NULL
 # create a date list
 local({
   days <- GetMonthLastDate(yyyymm) %>% seq(1, ., 1) %>% formatC(width=2, flag="0")
-  ymd <- as.Date(str_c(str_sub(yyyymm, 1, 4), str_sub(yyyymm, 5, 7), days, sep="-"))
+#  ymd <- as.Date(str_c(str_sub(yyyymm, 1, 4), str_sub(yyyymm, 5, 7), days, sep="-"))
+  ymd <- str_c(str_sub(yyyymm, 1, 4), str_sub(yyyymm, 5, 7), days, sep="-")
   df_calender <<- data.frame(ymd, stringsAsFactors=F)
 })
 local({
@@ -84,27 +87,24 @@ local({
 df_attendance <- df_attendance %>% filter(name != "")
 df_attendance$ymd <- as.Date(format(as.Date(df_attendance$日時), "%Y-%m-%d"))
 df_attendance$time <- format(as.POSIXct(df_attendance$日時), "%H:%M:%S")
-# 5分単位にまるめ
-df_attendance$roundup_time <- format(align.time(as.POSIXct(df_attendance$日時), 5*60), "%H:%M:%S")
-df_attendance$rounddown_time <- format(align.time(as.POSIXct(df_attendance$日時) - 5*60, 5*60), "%H:%M:%S")
-df_output <- NULL
 df_output_by_name_all <- NULL
 name_list <- df_attendance %>% distinct(name, .keep_all=F) %>% arrange(name)
 # group by name
 local({
   for (i in 1:nrow(name_list)){
     temp <- df_attendance %>% filter(name == name_list[i, 1]) %>% arrange(ymd)
-    temp_max <- temp %>% group_by(ymd) %>% filter(time==max(time)) %>% select(name, ymd, clockout=time, rounddown_time)
-    temp_min <- temp %>% group_by(ymd) %>% filter(time==min(time)) %>% select(name, ymd, clockin=time, roundup_time)
+    temp_max <- temp %>% group_by(ymd) %>% filter(time==max(time)) %>% select(name, ymd, clockout=time)
+    temp_min <- temp %>% group_by(ymd) %>% filter(time==min(time)) %>% select(name, ymd, clockin=time)
     temp_min_max <- full_join(temp_min, temp_max, by=c("name", "ymd"))
     # group by date
-    df_output <<- apply(temp_min_max, 1, GroupbyName) %>% c(df_output, .)
-    df_output_by_name <- apply(temp_min_max, 1, GroupbyName)
-    write.csv(do.call(rbind, df_output_by_name), str_c(output_path, "/", name_list[i, 1], ".csv"), row.names=F, fileEncoding="cp932")
-    # output other than work days
-    temp_output_by_name <- do.call(rbind, df_output_by_name)
-    temp_output_by_name$ymd <- as.Date(temp_output_by_name$年月日)
-    df_output_by_name_alldays <- left_join(df_calender, temp_output_by_name, by="ymd")
+    df_output_by_name <- apply(temp_min_max, 1, GroupbyName) %>% do.call(rbind, .) %>%
+                            right_join(df_calender, by="ymd") %>% select(c("入室", "退室"))
+    # Remove numbers and spaces from file names
+    output_file_name <- str_replace_all(name_list[i, 1], pattern="[0-9|\\s|　]", "")
+    if (output_file_name == ""){
+      output_file_name <- name_list[i, 1]
+    }
+    write.table(df_output_by_name, str_c(output_path, "/", output_file_name, ".txt"), sep="\t", na="",
+              row.names=F, col.names=F, fileEncoding="cp932")
   }
 })
-write.csv(do.call(rbind, df_output), str_c(output_path, "/", yyyymm, ".csv"), row.names=F, fileEncoding="cp932")
